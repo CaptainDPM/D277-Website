dc907f2ad734a8d98e325e04c07edeb22c5d9de5
CaptainDPM <100872653+CaptainDPM@users.noreply.github.com>
2023-06-05 20:17:52 -1000

    Adjusted some minor stuff and trying to add music

--- verify.js
+++ verify.js
@@ -2,9 +2,9 @@ function emailMatch() {
   var email1 = document.getElementById("email1").value
   var email2 = document.getElementById("email2").value
 
-  if (email1 !=== email2) {
-    alert("Emails do not match.")
+  if (email1 === email2) {
+    prompt("Emails match.");
   } else {
-    alert("Emails match.")
+    prompt("Emails do not match.");
   }
 }
