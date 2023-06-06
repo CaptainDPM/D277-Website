72a330b5d2b5198859938661f6425253893be181
CaptainDPM <100872653+CaptainDPM@users.noreply.github.com>
2023-06-05 20:49:56 -1000

    Properly adjusted verification of email form

--- verify.js
+++ verify.js
@@ -1,10 +1,8 @@
-function emailMatch() {
-  var email1 = document.getElementById("email1").value
-  var email2 = document.getElementById("email2").value
-
-  if (email1 === email2) {
-    prompt("Emails match.");
-  } else {
-    prompt("Emails do not match.");
+function validateForm() {
+  let email1 = document.getElementById("email1");
+  let email2 = document.getElementById("email2");
+  if (email1 !== email2) {
+    alert("Emails do not match.");
+    return false;
   }
 }
